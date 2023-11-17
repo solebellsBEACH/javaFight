18d0e4dd609693ec91c21bbf34c918e286fc7d88
solebellsBEACH <lucassxxavier@gmail.com>
2023-11-17 16:36:48 -0300

    feat: adding mother class Personagem

---
+++ src/personagens/Personagem.java
@@ -0,0 +1,28 @@
+package personagens;
+
+import armas.Arma;
+
+public class Personagem {
+	
+	String name;
+	Arma arma;
+	
+	public Personagem(String name, Arma arma) {
+		super();
+		this.name = name;
+		this.arma = arma;
+	}
+	
+	public void desenhar() {
+		
+	}
+	public void falar() {
+		
+	}
+	public void arma() {
+		
+	}
+	public void setArma() {
+		
+	}
+}
